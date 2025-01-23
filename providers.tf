6ba6578bb6e754f7e3ee5ed9e6feba808bbcba52
macinsight <git@macinsight.net>
2025-01-23 08:34:59 +0100

    Add providers

--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "terraform-provider-openstack/openstack"
       version = "~> 1.50.0"  # Use the desired version
     }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -11,8 +15,11 @@ provider "openstack" {
   auth_url           = "https://keystone.services.a.regiocloud.tech"
   user_name          = "hergemoeller"
   password           = var.os_password
-  project_name       = "b1systems-sandbox"
   user_domain_name   = "b1systems"
   project_domain_id  = "b2341addab75466ab4c0fefb4fd07f6a"
   region             = "RegionA"
+}
+
+provider "cloudflare" {
+  api_token = var.cf_api_token
 }
