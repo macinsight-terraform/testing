e8297368744065033243e7c19fe3faa0ce96bf3d
macinsight <git@macinsight.net>
2025-01-23 20:37:32 +0100

    Add prefix to hosts

--- compute.tf
+++ compute.tf
@@ -1,7 +1,7 @@
 resource "openstack_compute_instance_v2" "nodes" {
   for_each = toset(var.nodes)
 
-  name       = each.value
+  name            = format("hergemoeller-%s", each.value) # Add prefix to name
   image_name      = "Ubuntu 22.04"
   flavor_name     = "SCS-8V-16-50"
   key_pair        = "Hergemoeller"
