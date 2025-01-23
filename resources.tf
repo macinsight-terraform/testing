be7aed18ccf528b5aeca34a4f25fe6ec4aac1bbf
macinsight <git@macinsight.net>
2025-01-23 08:35:17 +0100

    Add resources to demo spinning up an openstack instance

--- resources.tf
+++ resources.tf
@@ -10,3 +10,32 @@ resource "openstack_compute_instance_v2" "k3s-master" {
     name = "hergemoeller_net"
   }
 }
+
+resource "openstack_networking_floatingip_v2" "fip" {
+  pool = "public"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "fip_association" {
+  floating_ip = openstack_networking_floatingip_v2.fip.address
+  instance_id = openstack_compute_instance_v2.k3s-master.id
+}
+
+resource "cloudflare_record" "k3s_master" {
+  zone_id = var.cf_zone_id 
+  name = var.cf_domain 
+  type = "A"
+  value = openstack_networking_floatingip_v2.fip.address
+  ttl = 300
+  proxied = false
+}
+
+output "k3s_master_accessip" {
+  value = openstack_compute_instance_v2.k3s-master.access_ip_v4
+  description = "Assigned access-ip of node"
+}
+
+
+output "floating_ip" {
+  value = openstack_networking_floatingip_v2.fip.address
+  description = "Floating IP of k3s master"
+}
