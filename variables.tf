067a237e678a5f20db8c77b66a446b79458821f2
macinsight <git@macinsight.net>
2025-01-23 08:35:51 +0100

    Add variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,23 @@ variable "os_password" {
   description = "Openstack password"
   type = string
 }
+variable "cf_api_token" {
+  description = "Cloudflare token"
+  type = string
+}
+
+variable "cf_zone_id" {
+  description = "Cloudflare Zone ID"
+  type = string
+}
+
+variable "cf_domain" {
+  description = "Cloudflare Domain"
+  type = string
+}
+
+variable "private_ssh_key" {
+  description = "Private SSH key for inter-instance access"
+  type = string
+  sensitive = true
+}
